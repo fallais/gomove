2e2f12d1a0eba90bddc24308c5d739ee2285a7bd
unknown <francois.allais@hotmail.com>
2025-08-22 15:40:15 +0200

    core: wip

--- internal/cmd/config/show/main.go
+++ internal/cmd/config/show/main.go
@@ -1,48 +1,30 @@
 package show
 
 import (
+	"bytes"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
 func Run(cmd *cobra.Command, args []string) {
-	fmt.Println("GoMove Configuration Status")
-	fmt.Println("===========================")
+	// Get all settings from Viper
+	allSettings := viper.AllSettings()
 
-	// Show config file location
-	configFile := viper.ConfigFileUsed()
-	if configFile == "" {
-		home, err := os.UserHomeDir()
-		if err == nil {
-			defaultConfigFile := filepath.Join(home, ".gomove", "config.yaml")
-			if _, err := os.Stat(defaultConfigFile); err == nil {
-				configFile = defaultConfigFile
-			} else {
-				configFile = "No config file found"
-			}
-		}
+	// Create a custom YAML encoder with 2-space indentation
+	var buf bytes.Buffer
+	encoder := yaml.NewEncoder(&buf)
+	encoder.SetIndent(2) // Set indentation to 2 spaces
+
+	err := encoder.Encode(allSettings)
+	if err != nil {
+		fmt.Printf("Error formatting configuration as YAML: %v\n", err)
+		return
 	}
+	encoder.Close()
 
-	fmt.Printf("Config file: %s\n", configFile)
-	fmt.Println()
-
-	// Show current settings
-	fmt.Println("Current Settings:")
-	fmt.Printf("  Interval: %d seconds\n", viper.GetInt("interval"))
-	fmt.Printf("  Distance: %d pixels\n", viper.GetInt("distance"))
-	fmt.Printf("  Debug: %t\n", viper.GetBool("debug"))
-
-	logfile := viper.GetString("logfile")
-	if logfile == "" {
-		logfile = "stdout"
-	}
-	fmt.Printf("  Log output: %s\n", logfile)
-
-	fmt.Println()
-	fmt.Println("To start the mouse mover, run: gomove start")
-	fmt.Println("To initialize a config file, run: gomove config init")
+	// Output the YAML directly without any headers
+	fmt.Print(buf.String())
 }
